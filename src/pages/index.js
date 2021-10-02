c98ea2e7cb3416cb53cd0f1a39080b97c20ea666
OliveBounty <oliveodobounty@gmail.com>
2021-10-03 01:04:21 +0900

    update 1

--- src/pages/index.js
+++ src/pages/index.js
@@ -101,21 +101,21 @@ const IndexPage = () => {
   return (
     <main style={pageStyles}>
       <title>Home Page</title>
-      <h1 style={headingStyles}>
+      {/* <h1 style={headingStyles}>
         Congratulations
         <br />
         <span style={headingAccentStyles}>— you just made a Gatsby site! </span>
         <span role="img" aria-label="Party popper emojis">
           🎉🎉🎉
         </span>
-      </h1>
-      <p style={paragraphStyles}>
+      </h1> */}
+      {/* <p style={paragraphStyles}>
         Edit <code style={codeStyles}>src/pages/index.js</code> to see this page
         update in real-time.{" "}
         <span role="img" aria-label="Sunglasses smiley emoji">
           😎
         </span>
-      </p>
+      </p> */}
       <ul style={listStyles}>
         <li style={docLinkStyle}>
           <a
